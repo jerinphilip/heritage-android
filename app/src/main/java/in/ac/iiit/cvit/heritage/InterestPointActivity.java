1c54ae02a6af0a40af6dbbdfb1f3a2a626286d33
Anshuman Majumdar <anshuman.py@gmail.com>
2016-08-28 20:09:31 +0530

    Ignore: minor changes

--- app/src/main/java/in/ac/iiit/cvit/heritage/InterestPointActivity.java
+++ app/src/main/java/in/ac/iiit/cvit/heritage/InterestPointActivity.java
@@ -6,11 +6,14 @@ import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 
 public class InterestPointActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
     private CollapsingToolbarLayout collapsingToolbarLayout;
+
+    private static final String LOGTAG = "Heritage";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,8 +22,10 @@ public class InterestPointActivity extends AppCompatActivity {
 
         Intent intent = getIntent();
         String text_interest_point = intent.getStringExtra("interest_point");
+        Log.i(LOGTAG, text_interest_point);
 
         toolbar = (Toolbar) findViewById(R.id.coordinatorlayout_toolbar);
+        toolbar.setTitle(text_interest_point);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
--- app/src/main/java/in/ac/iiit/cvit/heritage/InterestPointsFragment.java
+++ app/src/main/java/in/ac/iiit/cvit/heritage/InterestPointsFragment.java
@@ -1,6 +1,5 @@
 package in.ac.iiit.cvit.heritage;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -8,9 +7,7 @@ import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
-import android.view.GestureDetector;
 import android.view.LayoutInflater;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -34,19 +31,6 @@ public class InterestPointsFragment extends Fragment {
 
         ArrayList<InterestPoint> interestPoints = LoadPackage(packageName);
 
-/*
-        final GridView gridview_interest_points = (GridView) root.findViewById(R.id.gridview_interest_points);
-        final GridViewAdapter gridViewAdapter = new GridViewAdapter(getActivity(), R.layout.fragment_interest_points, interestPoints);
-        gridview_interest_points.setAdapter(gridViewAdapter);
-        gridview_interest_points.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                InterestPoint interestPoint = gridViewAdapter.getItem(position);
-                interestPoint.get("title");
-            }
-        });
-*/
-
         recyclerView = (RecyclerView) root.findViewById(R.id.recyclerview_interest_points);
         recyclerView.setHasFixedSize(true);
         recyclerViewLayoutManager = new LinearLayoutManager(getActivity());
@@ -69,23 +53,6 @@ public class InterestPointsFragment extends Fragment {
                     }
                 })
         );
-
-/*
-        recyclerView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                int position = recyclerView.getChildLayoutPosition(v);
-                RecyclerView.ViewHolder viewHolder = recyclerView.findViewHolderForLayoutPosition(position);
-
-                TextView textView = (TextView) viewHolder.itemView.findViewById(R.id.cardview_text);
-                String text = textView.getText().toString();
-
-                Intent intent_interest_point = new Intent(getActivity(), InterestPointActivity.class);
-                intent_interest_point.putExtra("interest_point", text);
-                startActivity(intent_interest_point);
-            }
-        });
-*/
 
         return root;
     }
