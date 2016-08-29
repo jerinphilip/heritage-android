a814ea05809f432e6ab7ba5cda9172287a1f7dcb
Jerin Philip <monu1618@gmail.com>
2016-08-29 22:48:43 +0530

    Added Location to NearbyPointsFragment
    Location works within NearbyPoints Fragment, currently using Google API
    Services.
    TODO: Code Refactoring
    
    There is a redundancy, both NearbyPointsFragment and
    InterestPointsFragment requires LoadPackage, and package is being read
    every time the intent changes. It's ideal to store this in memory and
    reuse the code.

--- app/src/main/java/in/ac/iiit/cvit/heritage/NearbyPointsFragment.java
+++ app/src/main/java/in/ac/iiit/cvit/heritage/NearbyPointsFragment.java
@@ -1,19 +1,216 @@
 package in.ac.iiit.cvit.heritage;
 
+import android.content.Context;
+import android.content.IntentSender;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class NearbyPointsFragment extends Fragment {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 
-    private static final String LOGTAG = "Heritage";
+import java.util.ArrayList;
+
+
+public class NearbyPointsFragment extends Fragment implements ConnectionCallbacks, OnConnectionFailedListener, LocationListener{
+
+    private static final String LOGTAG = "Heritage:Nearby";
+    //Define a request code to send to Google Play services
+    private final static int CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
+    private GoogleApiClient mGoogleApiClient;
+    private LocationRequest mLocationRequest;
+    private double currentLatitude;
+    private double currentLongitude;
+
+    private ArrayList<InterestPoint> InterestPoints;
+
+    private Context _context;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_nearby_points, container, false);
-
+        _context = getActivity();
+        mGoogleApiClient = null;
+        createLocationClients();
+        String packageName = getActivity().getIntent().getStringExtra("package");
+        InterestPoints = LoadPackage(packageName);
         return root;
+    }
+
+    @Override
+    public void onStart() {
+        mGoogleApiClient.connect();
+        super.onStart();
+    }
+
+    @Override
+    public void onStop() {
+        mGoogleApiClient.disconnect();
+        super.onStop();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        //Now lets connect to the API
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.v(this.getClass().getSimpleName(), "onPause()");
+
+
+        //Disconnect from API onPause()
+        if (mGoogleApiClient.isConnected()) {
+            LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
+            mGoogleApiClient.disconnect();
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {}
+
+
+    private void createLocationClients(){
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(_context)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
+        // Create the LocationRequest object
+        mLocationRequest = LocationRequest.create()
+                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+                .setInterval(10 * 1000)        // 10 seconds, in milliseconds
+                .setFastestInterval(1 * 1000); // 1 second, in milliseconds
+
+        Log.d(LOGTAG, "Clients Created");
+    }
+
+    @Override
+    public void onConnected(Bundle bundle){
+        Log.d(LOGTAG, "Running onConnected");
+        if (ActivityCompat.checkSelfPermission(_context, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(_context, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            Log.d(LOGTAG, "Permission Denied");
+            return;
+        }
+        Location location = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+
+        if (location == null) {
+            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+
+        } else {
+            //If everything went fine lets get latitude and longitude
+            currentLatitude = location.getLatitude();
+            currentLongitude = location.getLongitude();
+
+            // Display lat long on screen
+            /*
+            setContentView(R.layout.activity_main);
+            TextView textView = (TextView) findViewById(R.id.coordinates);
+            textView.setText("lattitude: " + currentLatitude + " longitude: " + currentLongitude);
+            */
+            Log.d(LOGTAG, "Creating toast, onConnected");
+            Toast.makeText(_context, currentLatitude + " WORKS " + currentLongitude + "", Toast.LENGTH_LONG).show();
+        }
+
+        boolean mRequestingLocationUpdates = true;
+        if (mRequestingLocationUpdates) {
+            startLocationUpdates();
+        }
+    }
+
+    protected void startLocationUpdates() {
+        Log.d(LOGTAG, "Calling startLocationUpdates");
+        if (ActivityCompat.checkSelfPermission(_context, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(_context, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        LocationServices.FusedLocationApi.requestLocationUpdates(
+                mGoogleApiClient, mLocationRequest, this);
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+            /*
+             * Google Play services can resolve some errors it detects.
+             * If the error has a resolution, try sending an Intent to
+             * start a Google Play services activity that can resolve
+             * error.
+             */
+        if (connectionResult.hasResolution()) {
+            try {
+                // Start an Activity that tries to resolve the error
+                connectionResult.startResolutionForResult(getActivity(), CONNECTION_FAILURE_RESOLUTION_REQUEST);
+                    /*
+                     * Thrown if Google Play services canceled the original
+                     * PendingIntent
+                     */
+            } catch (IntentSender.SendIntentException e) {
+                // Log the error
+                e.printStackTrace();
+            }
+        } else {
+                /*
+                 * If no resolution is available, display a dialog to the
+                 * user with the error.
+                 */
+            Log.e("Error", "Location services connection failed with code " + connectionResult.getErrorCode());
+        }
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        currentLatitude = location.getLatitude();
+        currentLongitude = location.getLongitude();
+
+        // Display lat long on screen
+        /*
+        setContentView(R.layout.activity_main);
+        TextView textView = (TextView) findViewById(R.id.coordinates);
+        textView.setText(currentLatitude + " WORKS " + currentLongitude);
+        */
+        Log.d(LOGTAG, "OnLocationChanged:"+"("+currentLatitude+","+currentLongitude+")");
+        Toast.makeText(_context, currentLatitude + " WORKS " + currentLongitude + "", Toast.LENGTH_LONG).show();
+    }
+
+    private ArrayList<InterestPoint> LoadPackage(String packageName){
+        PackageReader reader;
+        packageName = packageName.toLowerCase();
+        reader = new PackageReader(packageName);
+        ArrayList<InterestPoint> interestPoints = reader.getContents();
+        return interestPoints;
     }
 }
